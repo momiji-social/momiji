62e998996dd8d89b2459bdeedc1a921cd1bc5f4e
yugoatobe <yugoatobe0330@gmail.com>
2025-02-04 19:03:46 +0900

    Initial commit

---
+++ Momiji/MomijiApp.swift
@@ -0,0 +1,33 @@
+//
+//  MomijiApp.swift
+//  Momiji
+//
+//  Created by yugoatobe on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MomijiApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
---
+++ MomijiTests/MomijiTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomijiTests.swift
+//  MomijiTests
+//
+//  Created by yugoatobe on 2/4/25.
+//
+
+import Testing
+@testable import Momiji
+
+struct MomijiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
